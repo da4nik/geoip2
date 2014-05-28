049ce30109c1d7060fd296e3e985dc2e0348354a
Steve Hodgkiss <steve@hodgkiss.me>
2014-05-28 11:54:08 +1000

    Raise with helpful error if database file cannot be found

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,8 @@ require 'minitest/autorun'
 
 require 'geoip2'
 
+unless File.exist?('GeoLite2-City.mmdb')
+  raise "GeoLite2-City.mmdb doesn't exist. Run rake download_free_database to get it"
+end
+
 GeoIP2.file('GeoLite2-City.mmdb')
